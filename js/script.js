79e2fc05ec40b1c6e23fbd73df4f06f1a0dd4e6b
lucas <accaunoff99@gmail.com>
2025-02-22 05:28:09 +0900

    install and learn eslint

--- js/script.js
+++ js/script.js
@@ -47,8 +47,8 @@ window.addEventListener("DOMContentLoaded", () => {
     function getTimeRemaining(endTime) {
         const time = Date.parse(endTime) - Date.parse(new Date()), // belgilangan vaqtdan hozirgi vaqtni ayirib beradi
             days = Math.floor(time / (1000 * 60 * 60 * 24)),
-            hours = Math.floor((time / (1000 * 60 * 60)) % 24)
-        minutes = Math.floor((time / (1000 * 60)) % 60),
+            hours = Math.floor((time / (1000 * 60 * 60)) % 24),
+            minutes = Math.floor((time / (1000 * 60)) % 60),
             seconds = Math.floor((time / (1000)) % 60);
 
         return {
@@ -73,8 +73,8 @@ window.addEventListener("DOMContentLoaded", () => {
             days = timer.querySelector('#days'),
             hours = timer.querySelector('#hours'),
             minutes = timer.querySelector('#minutes'),
-            seconds = timer.querySelector('#seconds');
-        timeInterval = setInterval(updateClock, 1000);
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
 
         updateClock() // agar projectimizda loader bolmsa pastdagi clockimiz 12 sec turib ishlaydi. bu hunuk ko'rinihsga olib keladi.shuni oldini olish un updateClock() ni tashqarida ham bir chaqirib olamiz!
 
